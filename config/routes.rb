4487f5104c01c9a28b3e78a74582d2331caad49c
hydroxyzinum <przemek.prokopczuk1@gmail.com>
2016-10-06 20:57:15 +0000

    Basic log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
-
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: "static_pages#help"
   get '/about', to: "static_pages#about"
   get '/signup', to: "users#new"
-  get '/login', to: "static_pages#login"
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
   resources :users
   
 end
