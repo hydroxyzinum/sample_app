1d1837c876d8ea9f453edab598e85920f265ea16
hydroxyzinum <przemek.prokopczuk1@gmail.com>
2016-10-16 17:29:14 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: "static_pages#help"
@@ -10,5 +14,6 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#destroy"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   
 end
