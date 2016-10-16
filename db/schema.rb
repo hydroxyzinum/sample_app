68a70166058df2467cbb77f62e2339a22772fc1d
hydroxyzinum <przemek.prokopczuk1@gmail.com>
2016-10-16 14:11:27 +0000

    Fixed account activation 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016125339) do
+ActiveRecord::Schema.define(version: 20161016133147) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
